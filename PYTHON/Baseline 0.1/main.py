2649b1f84521a2c2784fd9bde84518666cb6458e
JulienRg <julien.rousselgalle@gmail.com>
2022-11-07 14:18:39 +0100

    version 2.0

---
+++ PYTHON/Baseline 0.1/main.py
@@ -0,0 +1,17 @@
+import argparse
+"""
+    file:   main.py
+    subject:    call of functions and setting parameters
+    Author:  AVS7
+    Creation:    7/11/2022
+    Last Update: 7/11/2022
+    Update Note:
+        7/11/2022 : Creation
+
+"""
+
+
+if __name__ == '__main__':
+    # set parameters
+    path = argparse.ArgumentParser()
+
