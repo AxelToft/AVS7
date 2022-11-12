81594ad6835f54e0a2536f45a64ffc7d976ee78e
kajmoerk <kmark19@student.aau.dk>
2022-11-12 19:54:30 +0100

    Updated export_json to export proper JSON format

--- PYTHON/Baseline 0.1/export_json.py
+++ PYTHON/Baseline 0.1/export_json.py
@@ -17,6 +17,11 @@ import numpy as np
 
 from video import video as vd
 
+def initialize_json(file_name):
+    with open(file_name, 'w+') as file:
+        data = "{\"results\": []}"
+        file.write(data)
+        file.close()
 
 def export_json(video, path, file_name):
     """
@@ -30,8 +35,9 @@ def export_json(video, path, file_name):
     Returns:
 
     """
-    #TODO : Clear file at the beginning
-    with open(file_name, 'a') as outfile:
+
+    with open(file_name, 'r+') as file:
+        file_data = json.load(file)
         if video.enter_frames_numbers.size != 0:
             enter_frames_numbers = video.enter_frames_numbers.tolist()
         else:
@@ -44,12 +50,44 @@ def export_json(video, path, file_name):
             fish_count_frames =  video.fish_count_frames.tolist()
         else:
             fish_count_frames = []
-        data = {
-            video.name: {'enter_frame': enter_frames_numbers,
-                         'exit_frame': exit_frames_numbers, 'fish_count': video.count_fish}}
+        file_data["results"].append({
+            "video": video.name,
+            "enter_frames": enter_frames_numbers,
+            "exit_frames": exit_frames_numbers,
+            "fish_count": video.count_fish
+        })
+        file.seek(0)
+        json.dump(file_data,file, indent=4)
+        #file.write(',\n')
+
+
+    #TODO : Clear file at the beginning
+    """with open(file_name, 'a') as outfile:
+        if video.enter_frames_numbers.size != 0:
+            enter_frames_numbers = video.enter_frames_numbers.tolist()
+        else:
+            enter_frames_numbers = []
+        if video.exit_frames_numbers.size != 0:
+            exit_frames_numbers = video.exit_frames_numbers.tolist()
+        else:
+            exit_frames_numbers = []
+        if  video.fish_count_frames.size != 0:
+            fish_count_frames =  video.fish_count_frames.tolist()
+        else:
+            fish_count_frames = []
+        data["results"].append({
+            "video": video.name,
+            "enter_frames": enter_frames_numbers,
+            "exit_frames": exit_frames_numbers,
+            "fish_count_frames": fish_count_frames
+        })
+
+        #data = {
+        #    video.name: {'enter_frame': enter_frames_numbers,
+        #                 'exit_frame': exit_frames_numbers, 'fish_count': video.count_fish}}
 
         json.dump(data, outfile,indent=3)
-        outfile.write(',\n')
+        outfile.write(',\n')"""
 
 
 
